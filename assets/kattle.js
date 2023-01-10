99739d13091e990d7fc4300c8eb353099682840f
stanislavBenet <stanislav.benet@gmail.com>
2023-01-10 17:30:42 +0200

    Add JS-code for Kattle

---
+++ assets/kattle.js
@@ -0,0 +1,38 @@
+function FuncForKattle() {
+   this.work = function () {
+      if (this.currentVolumeWater > 0 && this.currentVolumeWater <= 5) {
+         return "Kattle start boiling";
+      }
+      return "Kattle is empty, please add water";
+   };
+   this.addWater = function (value) {
+      if (
+         this.currentVolumeWater <= this.volume &&
+         this.currentVolumeWater + value <= this.volume
+      ) {
+         return `${"Your kattle has"} ${(this.currentVolumeWater +=
+            this.volume)} ${"liter!"}`;
+      }
+      return `${"You want to add too mach water! Enough!"} ${"Your kattle has"} ${
+         this.currentVolumeWater
+      } ${"liter"}`;
+   };
+   this.subWater = function (value) {
+      if (this.currentVolumeWater > 0 && this.currentVolumeWater - this.volume >= 0) {
+         return `${"Your kattle has"} ${(this.currentVolumeWater -= value)} ${"litr"}`;
+      }
+      if (this.currentVolumeWater - value < 0 && this.currentVolumeWater == 0) {
+         return "Your kattle is empty";
+      }
+      return `${"You lose all water.Now your kattle is empty"}`;
+   };
+}
+function Kattle(color, volume) {
+   this.color = color;
+   this.volume = volume;
+   this.currentVolumeWater = 0;
+}
+
+Kattle.prototype = new FuncForKattle();
+
+const myKattle = new Kattle("blue", 5);
